e9bb555b2a3ede7d5662e12d3dd04f008512061a
Elizaveta-Valintain <retenets@gmail.com>
2022-06-11 18:59:24 +0300

    React Lesson 071 (setState, local state)

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -10,8 +10,8 @@ const ProfileInfo = (props) => {
         <div>
             <div>
 
-                    <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'
-                         alt='profile img'/>
+                    {/*<img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'*/}
+                    {/*     alt='profile img'/>*/}
 
             </div>
             <div className={style.discriptionBlock}>
