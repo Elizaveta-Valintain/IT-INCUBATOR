64ed65328fe03e96e4fe203abfa02e4bb08b5213
Elizaveta-Valintain <retenets@gmail.com>
2022-06-11 18:59:24 +0300

    React Lesson 071 (setState, local state)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import s from './Profile.module.css';
 import ProfileInfo from "./MyPosts/ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import ProfileStatus from "./MyPosts/ProfileStatus";
 
 const Profile = (props) => {
     return (
         <div className={s.content}>
             <ProfileInfo profile={props.profile}/>
+            <ProfileStatus status='Hi, this is status line' />
             <MyPostsContainer />
         </div>
     );
