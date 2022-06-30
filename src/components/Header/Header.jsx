0926594e9c2ee453aba3fdef6d9f7865d13d0961
Elizaveta-Valintain <retenets@gmail.com>
2022-06-30 17:19:45 +0300

    React Lesson 079 (stopSubmit(redux-form))

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
             <img src='https://i.pinimg.com/736x/de/a0/f3/dea0f3b7f480b1151c08db4a402a43b9.jpg' alt='img header'/>
             <div className={s.loginBlock}>
                 {props.isAuth
-                    ? props.login
+                    ? <div>{props.login} - <button onClick={props.logout}>LogOut</button></div>
                     : <NavLink to={'login'}>LOGIN</NavLink>
                 }
             </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,11 +9,12 @@ class ProfileContainer extends React.Component {
 
     componentDidMount() {
         // let userId = this.props.match.params.userId
-        // if (!userId) {
-        //     userId = 2
-        // }
+        let userId
+         if (!userId) {
+             userId = this.props.authorizedUserId
+         }
 
-            let userId = 16750
+          //  let userId = 16750
 
         this.props.getUserProfile(userId);
         this.props.getStatus(userId)
@@ -32,7 +33,9 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => {
     return {
         profile: state.profilePage.profile,
-        status : state.profilePage.status
+        status : state.profilePage.status,
+        authorizedUserId: state.auth.userId,
+        isAuth: state.auth.isAuth
     }
 }
 
